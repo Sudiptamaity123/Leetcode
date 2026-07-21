c4842095945ad2cf62d15154d0698a003f3d7d52
Sudipta Maity <80695244+Sudiptamaity123@users.noreply.github.com>
2026-07-21 11:17:16 +0530

    Time: 0 ms (100%), Space: 8.8 MB (68.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(auto it:s){
+            if(it=='[' || it=='('|| it=='{')
+                st.push(it);
+            else{
+                if(!st.empty() && st.top()=='('&&it==')')
+                    st.pop();
+                else if(!st.empty() && st.top()=='{'&&it=='}')
+                    st.pop();
+                else if(!st.empty() && st.top()=='['&&it==']')
+                    st.pop();
+                else 
+                    return false;
+            }
+        }
+        if(!st.empty())
+            return false;
+        return true;
+    }
+};
